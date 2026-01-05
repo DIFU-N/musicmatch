dd635441b73fdcdfb0a7da9d5760a638f6c63db0
it's <chukwudifunsofor@gmail.com>
2026-01-05 23:12:38 +0100

    feat: register service and add mutation types

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHttpClient<SpotifyService>();
 
 builder.Services.AddGraphQLServer().AddQueryType<Query>()
+.AddMutationType<Mutation>()
 .RegisterService<SpotifyService>();
 
 builder.Services.AddCors(options =>
